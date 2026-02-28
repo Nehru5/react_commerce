76920d475eb01103d4ee893cc59665b1434c2824
Nehru raja <nehruraja9485@gmail.com>
2026-02-28 10:56:19 +0530

    nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div className="nav">
+        <div className="left">
+          <h2>Cartify</h2>
+        </div>
+        <div className="right">
+          <ol>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Orders</li>
+          </ol>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Nav
