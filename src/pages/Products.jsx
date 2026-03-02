98165d9230739a330101c1c137686a48734a5927
Nehru raja <nehruraja9485@gmail.com>
2026-03-02 11:46:51 +0530

    Products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Nav from '../components/Nav'
-
+import axios from 'axios'
 const Products = () => {
+  const [products,setProducts] = useState([])
+  useEffect(()=>{
+      axios.get("http://localhost:5000/products")
+      .then(x=>setProducts(x.data))
+      .catch(err=>console.log(err))
+  },[])
 
   return (
     <>
       <Nav/>
-      <h1>Products</h1>
+      <center><h1 style={{backgroundColor:"orange", padding:"20px"}}>Products</h1></center>
+
+      <div className="product_card">
+        {products.map((x)=>{
+          return <div className='p-c'>
+            <img src={x.img} alt="" />
+            <h3>{x.name}</h3>
+            <h3>Price: {x.price}</h3>
+            <button>View</button>
+          </div>
+      })}
+      </div>
     </>
   )
 }
